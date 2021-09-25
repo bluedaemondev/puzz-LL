e7a23a8aea3fb4e7055684cab38b7e6aea442d7c
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-09-25 19:09:20 -0300

    pendiente

---
+++ ProyectoCuatrimestral_LawlerLanosa/Assets/_scripts/movements/SimpleAdvance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAdvance : IMover
+{
+    float speed;
+    Transform myTransform;
+
+    public SimpleAdvance(Transform transf, float speed = 6)
+    {
+        this.speed = speed;
+        this.myTransform = transf;
+    }
+    public void Advance()
+    {
+        myTransform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+}
