b4075fb983621ba6a44999c6de2b8a65983827c8
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-09-27 16:31:10 -0300

    player input

--- ProyectoCuatrimestral_LawlerLanosa/Assets/_scripts/user_input/PlayerInput.cs
+++ ProyectoCuatrimestral_LawlerLanosa/Assets/_scripts/user_input/PlayerInput.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PlayerInput : MonoBehaviour
+public class PlayerInput : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
 {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
